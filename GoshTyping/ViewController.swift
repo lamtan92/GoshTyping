3c9d172b784d4e940076cfbcf008e8d18be97972
Tran Tan Lam <trankind@gmail.com>
2016-11-24 21:15:29 +0700

    init project

---
+++ GoshTyping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoshTyping
+//
+//  Created by Lam Tran on 11/24/16.
+//  Copyright © 2016 Tan Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
